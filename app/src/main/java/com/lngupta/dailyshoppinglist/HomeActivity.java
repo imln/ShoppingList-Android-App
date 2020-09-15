8b09c25c141ccc36f4e35a724fc4d9714d1b9747
LNGupta <campustray@gmail.com>
2020-09-15 18:19:01 +0530

    Login and register module added

---
+++ app/src/main/java/com/lngupta/dailyshoppinglist/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.lngupta.dailyshoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Daily Shooping List");
+    }
+}
